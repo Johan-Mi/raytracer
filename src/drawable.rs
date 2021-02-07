ace22c2c9e9b6a65085a5c9fa1141b7fccc84f6d
Johan-Mi <johanmi@protonmail.com>
2021-02-07 09:09:20 +0100

    Buffer file writes

--- src/drawable.rs
+++ src/drawable.rs
@@ -1,6 +1,6 @@
 use super::color::Color;
 use std::fs;
-use std::io::Write;
+use std::io::{BufWriter, Write};
 
 pub trait Drawable {
     const WIDTH: usize;
@@ -9,13 +9,17 @@ pub trait Drawable {
     fn get_color_at(&self, x: usize, y: usize) -> Color;
 
     fn write_ppm<P: AsRef<std::path::Path>>(&self, filename: P) {
-        let mut f = fs::File::create(filename).unwrap();
-        writeln!(f, "P6 {} {} 255", Self::WIDTH, Self::HEIGHT).unwrap();
+        let f = fs::File::create(filename).unwrap();
+        let mut buf = BufWriter::new(f);
+
+        writeln!(buf, "P6 {} {} 255", Self::WIDTH, Self::HEIGHT).unwrap();
         for y in 0..Self::HEIGHT {
             for x in 0..Self::WIDTH {
                 let color = self.get_color_at(x, y);
-                f.write(&[color.r, color.g, color.b]).unwrap();
+                buf.write(&[color.r, color.g, color.b]).unwrap();
             }
         }
+
+        buf.flush().unwrap();
     }
 }
