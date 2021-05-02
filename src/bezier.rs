fd03e3b826b810f0c4dde7d51abbe0e40e561155
Johan-Mi <johanmi@protonmail.com>
2021-05-02 09:50:18 +0200

    Avoid recalculating Bézier curves unncessearily

--- src/bezier.rs
+++ src/bezier.rs
@@ -15,15 +15,6 @@ fn eval_bezier_curve(points: &[Point3; 4], t: f32) -> Point3 {
     points[0] * b0 + points[1] * b1 + points[2] * b2 + points[3] * b3
 }
 
-fn eval_bezier_patch(points: &[[Point3; 4]; 4], u: f32, v: f32) -> Point3 {
-    let mut curve = [Point3::default(); 4];
-    for i in 0..4 {
-        curve[i] = eval_bezier_curve(&points[i], u);
-    }
-
-    eval_bezier_curve(&curve, v)
-}
-
 pub fn build_bezier_patch<'a>(
     points: &[[Point3; 4]; 4],
     material: &'a (dyn Material + Sync),
@@ -33,12 +24,15 @@ pub fn build_bezier_patch<'a>(
 
     let mut vertices = [[Point3::default(); DIVISIONS + 1]; DIVISIONS + 1];
     for u in 0..=DIVISIONS {
+        let mut curve = [Point3::default(); 4];
+        for i in 0..4 {
+            curve[i] =
+                eval_bezier_curve(&points[i], u as f32 / DIVISIONS as f32);
+        }
+
         for v in 0..=DIVISIONS {
-            vertices[u][v] = eval_bezier_patch(
-                points,
-                u as f32 / DIVISIONS as f32,
-                v as f32 / DIVISIONS as f32,
-            )
+            vertices[u][v] =
+                eval_bezier_curve(&curve, v as f32 / DIVISIONS as f32);
         }
     }
 
