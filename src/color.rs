00e0a736de1827e3c4003fc4a9b4d19a73510b64
Johan-Mi <johanmi@protonmail.com>
2021-06-28 22:09:04 +0200

    Remove repeated code in `to_rgb()`

--- src/color.rs
+++ src/color.rs
@@ -6,9 +6,7 @@ pub type Color = Vec3<f32>;
 pub fn to_rgb(mut color: Color, gamma: f32) -> [u8; 3] {
     let inv_gamma = 1.0 / gamma;
 
-    color.x = color.x.powf(inv_gamma);
-    color.y = color.y.powf(inv_gamma);
-    color.z = color.z.powf(inv_gamma);
+    color = color.map(|chan| chan.powf(inv_gamma));
 
     let mut sat: f32 = 1.0;
     let luma = color.dot(&Color {
@@ -17,23 +15,19 @@ pub fn to_rgb(mut color: Color, gamma: f32) -> [u8; 3] {
         z: 0.114,
     });
 
-    for n in 0..3 {
-        if color[n] > 1.0 {
-            sat = sat.min((luma - 1.0) / (luma - color[n]));
-        } else if color[n] < 0.0 {
-            sat = sat.min(luma / (luma - color[n]));
+    for chan in color {
+        if chan > 1.0 {
+            sat = sat.min((luma - 1.0) / (luma - chan));
+        } else if chan < 0.0 {
+            sat = sat.min(luma / (luma - chan));
         }
     }
 
     sat = sat.clamp(0.0, 1.0);
 
-    color.x = ((color.x - luma) * sat + luma).clamp(0.0, 1.0);
-    color.y = ((color.y - luma) * sat + luma).clamp(0.0, 1.0);
-    color.z = ((color.z - luma) * sat + luma).clamp(0.0, 1.0);
+    color = color.map(|chan| ((chan - luma) * sat + luma).clamp(0.0, 1.0));
 
-    [
-        (color.x * (256.0 - 1e-5)) as u8,
-        (color.y * (256.0 - 1e-5)) as u8,
-        (color.z * (256.0 - 1e-5)) as u8,
-    ]
+    let rgb = color.map(|chan| (chan * (256.0 - 1e-5)) as u8);
+
+    [rgb.x, rgb.y, rgb.z]
 }
