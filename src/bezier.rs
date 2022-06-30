1a70759289e5089b32de71172b0e4de22c1adc6f
Johan-Mi <johanmi@protonmail.com>
2022-06-30 11:55:47 +0200

    Remove unnecessary array indexing

--- src/bezier.rs
+++ src/bezier.rs
@@ -24,9 +24,8 @@ pub fn build_bezier_patch<'a>(
 
     let mut vertices = [[Point3::default(); DIVISIONS + 1]; DIVISIONS + 1];
     for (u, row) in vertices.iter_mut().enumerate() {
-        let curve = [0, 1, 2, 3].map(|i| {
-            eval_bezier_curve(&points[i], u as f32 / DIVISIONS as f32)
-        });
+        let curve =
+            points.map(|p| eval_bezier_curve(&p, u as f32 / DIVISIONS as f32));
 
         for (v, vert) in row.iter_mut().enumerate() {
             *vert = eval_bezier_curve(&curve, v as f32 / DIVISIONS as f32);
