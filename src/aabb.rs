90bffc809bf727cd4fd0a2c17d2fedb46b8b290a
Johan-Mi <johanmi@protonmail.com>
2023-04-15 23:01:05 +0200

    Make AABB collision more concise

--- src/aabb.rs
+++ src/aabb.rs
@@ -1,4 +1,4 @@
-use crate::{ray::Ray, Point3};
+use crate::{ray::Ray, Point3, Vec3};
 use std::ops::Range;
 
 #[derive(Clone)]
@@ -11,43 +11,24 @@ impl Aabb {
     pub fn collides(&self, ray: &Ray, t_range: Range<f32>) -> bool {
         let inv_d = 1.0 / ray.dir;
 
-        let (t0_x, t1_x) = if inv_d.x < 0.0 {
-            (self.maximum.x, self.minimum.x)
-        } else {
-            (self.minimum.x, self.maximum.x)
-        };
+        let t0 =
+            Vec3::select(inv_d.cmplt(Vec3::ZERO), self.maximum, self.minimum);
+        let t1 =
+            Vec3::select(inv_d.cmplt(Vec3::ZERO), self.minimum, self.maximum);
 
-        let (t0_y, t1_y) = if inv_d.y < 0.0 {
-            (self.maximum.y, self.minimum.y)
-        } else {
-            (self.minimum.y, self.maximum.y)
-        };
+        let t0 = (t0 - ray.origin) * inv_d;
+        let t1 = (t1 - ray.origin) * inv_d;
 
-        let (t0_z, t1_z) = if inv_d.z < 0.0 {
-            (self.maximum.z, self.minimum.z)
-        } else {
-            (self.minimum.z, self.maximum.z)
-        };
-
-        let t0_x = (t0_x - ray.origin.x) * inv_d.x;
-        let t1_x = (t1_x - ray.origin.x) * inv_d.x;
-
-        let t0_y = (t0_y - ray.origin.y) * inv_d.y;
-        let t1_y = (t1_y - ray.origin.y) * inv_d.y;
-
-        let t0_z = (t0_z - ray.origin.z) * inv_d.z;
-        let t1_z = (t1_z - ray.origin.z) * inv_d.z;
-
-        let t_min = t0_x.max(t_range.start);
-        let t_max = t1_x.min(t_range.end);
+        let t_min = t0.x.max(t_range.start);
+        let t_max = t1.x.min(t_range.end);
         let a = t_max > t_min;
 
-        let t_min = t0_y.max(t_min);
-        let t_max = t1_y.min(t_max);
+        let t_min = t0.y.max(t_min);
+        let t_max = t1.y.min(t_max);
         let b = t_max > t_min;
 
-        let t_min = t0_z.max(t_min);
-        let t_max = t1_z.min(t_max);
+        let t_min = t0.z.max(t_min);
+        let t_max = t1.z.min(t_max);
         let c = t_max > t_min;
 
         a & b & c
